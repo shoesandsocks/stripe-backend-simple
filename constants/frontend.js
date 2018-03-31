8f01e22f111cfc77de486f96acc0ab18a26e91e5
RL Brown <rlbrown72@gmail.com>
2018-03-31 10:50:16 -0400

    initial commit

---
+++ constants/frontend.js
@@ -0,0 +1,5 @@
+const FRONTEND_DEV_URLS = ['http://localhost:3000', 'http://localhost:8000']; // TODO: for blog
+
+const FRONTEND_PROD_URLS = ['https://www.pineandvine.com', 'https://pineandvine.com'];
+
+module.exports = process.env.NODE_ENV === 'production' ? FRONTEND_PROD_URLS : FRONTEND_DEV_URLS;
