534fed38901aebd57b4dac3c4b65badf72a9684c
RL Brown <rlbrown72@gmail.com>
2018-07-03 15:50:08 -0400

    new dev url

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,13 +1,12 @@
 const FRONTEND_DEV_URLS = [
   'http://localhost:3000',
   'http://localhost:8000',
-  'https://0f885ea0.ngrok.io',
+  'https://f9000564.ngrok.io',
 ]; // TODO: for blog
 
 const FRONTEND_PROD_URLS = [
   'https://www.pineandvine.com',
   'https://pineandvine.com',
-  'https://0f885ea0.ngrok.io',
 ];
 
 module.exports = process.env.NODE_ENV === 'production' ? FRONTEND_PROD_URLS : FRONTEND_DEV_URLS;
