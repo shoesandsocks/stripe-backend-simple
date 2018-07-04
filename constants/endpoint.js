b73d5b7b7b219106425bdcdf89a9a617d48f1114
RL Brown <rlbrown72@gmail.com>
2018-07-04 14:21:00 -0400

    temporary test swap on endpoint

--- constants/endpoint.js
+++ constants/endpoint.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
+// not sure how to flip this back and forth, when the code is ALWAYS in prod on Heroku.
+// temporary swap, then swap back and re-deploy?
+
 const STRIPE_ENDPOINT_SECRET =
   process.env.NODE_ENV === 'production'
-    ? process.env.STRIPE_LIVE_ENDPOINT_SECRET
+    ? process.env.STRIPE_TEST_ENDPOINT_SECRET
     : process.env.STRIPE_TEST_ENDPOINT_SECRET;
 
 const endpoint = STRIPE_ENDPOINT_SECRET;
