a74453f0cc8645681093e5c8f27f126d4a234a8c
RL Brown <rlbrown72@gmail.com>
2018-07-06 16:09:08 -0400

    forgot to swap to test secret!

--- constants/endpoint.js
+++ constants/endpoint.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const STRIPE_ENDPOINT_SECRET =
   process.env.NODE_ENV === 'production'
-    ? process.env.STRIPE_LIVE_ENDPOINT_SECRET
+    ? process.env.STRIPE_TEST_ENDPOINT_SECRET
     : process.env.STRIPE_TEST_ENDPOINT_SECRET;
 
 const endpoint = STRIPE_ENDPOINT_SECRET;
