5743c00a31b95312dd944b8e575185da48f7e80a
RL Brown <rlbrown72@gmail.com>
2018-07-11 13:06:01 -0400

    flip stripe secret also

--- constants/stripe.js
+++ constants/stripe.js
@@ -2,9 +2,11 @@ const configureStripe = require('stripe');
 
 require('dotenv').config();
 
+// same problem here TODO:
+
 const STRIPE_SECRET_KEY =
   process.env.NODE_ENV === 'production'
-    ? process.env.STRIPE_LIVE_SECRET
+    ? process.env.STRIPE_TEST_SECRET
     : process.env.STRIPE_TEST_SECRET;
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
