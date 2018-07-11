ce132ea08fa2aee34f7024b11a4c9e3c9626a0db
RL Brown <rlbrown72@gmail.com>
2018-07-11 13:40:16 -0400

    working - swap back to LIVE

--- constants/stripe.js
+++ constants/stripe.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const STRIPE_SECRET_KEY =
   process.env.NODE_ENV === 'production'
-    ? process.env.STRIPE_TEST_SECRET
+    ? process.env.STRIPE_LIVE_SECRET
     : process.env.STRIPE_TEST_SECRET;
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
