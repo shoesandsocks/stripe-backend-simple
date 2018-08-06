d0bdaceda917738dc8c80a984628aac843d2df60
RL Brown <rlbrown72@gmail.com>
2018-08-06 13:32:44 -0400

    confirm environment at heroku

--- constants/endpoint.js
+++ constants/endpoint.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 
 // not sure how to flip this back and forth, when the code is ALWAYS in prod on Heroku.
 // temporary swap, then swap back and re-deploy?
+console.log(`we are in ${process.env.NODE_ENV}`);
 
 const STRIPE_ENDPOINT_SECRET =
   process.env.NODE_ENV === 'production'
     ? process.env.STRIPE_LIVE_ENDPOINT_SECRET
     : process.env.STRIPE_TEST_ENDPOINT_SECRET;
 
-// const endpoint = STRIPE_ENDPOINT_SECRET;
-
-const endpoint = process.env.STRIPE_TEST_ENDPOINT_SECRET;
+const endpoint = STRIPE_ENDPOINT_SECRET;
 
 module.exports = endpoint;
