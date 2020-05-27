5e749f9835afca8a33cfdfde362a147fd7c11112
RL Brown <rich@pineandvine.com>
2020-05-27 15:41:11 -0400

    relax cors for a sec

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const CORS_WHITELIST = require('./constants/frontend');
+// const CORS_WHITELIST = require('./constants/frontend');
 
-const testOrigin = (o) => CORS_WHITELIST.indexOf(o) !== -1 || o === undefined;
+// const testOrigin = (o) => CORS_WHITELIST.indexOf(o) !== -1 || o === undefined;
 
-const corsOptions = {
-  origin: (origin, callback) => (testOrigin(origin) ? callback(null, true) : callback(new Error('Not allowed by CORS'))),
-};
+// const corsOptions = {
+//   origin: (origin, callback) => (testOrigin(origin) ? callback(null, true) : callback(new Error('Not allowed by CORS'))),
+// };
 
 const configureServer = (app) => {
-  app.use(cors(corsOptions));
-  app.use(bodyParser.raw({ type: '*/*' })); // see https://stripe.com/docs/webhooks
+  app.use(cors());
+  // app.use(cors(corsOptions));
+  app.use(bodyParser.raw({ type: "*/*" })); // see https://stripe.com/docs/webhooks
   // app.use(bodyParser.json());
 };
 
