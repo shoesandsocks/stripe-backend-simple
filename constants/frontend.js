1f86df4a17d9b91e4c3b7b03d613a0b40bb41199
RL Brown <rich@pineandvine.com>
2020-05-27 14:23:11 -0400

    oops

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,12 +1,10 @@
-const FRONTEND_DEV_URLS = [
-  "http://localhost:8080",
-  "https://b76bf8f9.ngrok.io",
-]; // TODO: for blog
+const FRONTEND_DEV_URLS = ["http://localhost:8080"]; // TODO: for blog
 
 const FRONTEND_PROD_URLS = [
   "https://www.pineandvine.com",
   "https://pineandvine.com",
   "https://www.rich-text.net",
+  "https://b76bf8f9.ngrok.io",
 ];
 
 module.exports =
