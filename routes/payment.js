7d8b13b9a78a296a60da1caaee07b55d1307df40
RL Brown <rich@pineandvine.com>
2020-05-29 15:57:58 -0400

    major stripe update; remove old routes

--- routes/payment.js
+++ routes/payment.js
@@ -15,11 +15,11 @@ const STRIPE_SECRET_ENDPOINT =
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 const liveEndpoint = STRIPE_SECRET_ENDPOINT;
-
-const publishableKey =
-  process.env.NODE_ENV === "production"
-    ? "pk_live_LOHrtxPBcZ5LnlG9KeCV6e9H"
-    : "pk_test_QBxssu5Kyu68z5R3grsjBgO0";
+// console.log(Object.keys(stripe));
+// const publishableKey =
+//   process.env.NODE_ENV === "production"
+//     ? "pk_live_LOHrtxPBcZ5LnlG9KeCV6e9H"
+//     : "pk_test_QBxssu5Kyu68z5R3grsjBgO0";
 
 const saveUser = require("../dbase/connect");
 const convert = require("../constants/convert");
@@ -46,123 +46,112 @@ const paymentApi = (app) => {
     });
   });
 
-  app.post("/", async (req, res) => {
-    console.log("handling POST");
-    // const obj = JSON.parse(req.body); // FIXME: this is a bad idea. hackable from F.E.
-    // Token is created using Stripe Checkout or Elements!
-    // Get the payment token ID submitted by the form:
-    const token = req.body.stripeToken; // Using Express
+  // app.post("/", async (req, res) => {
+  //   console.log("handling POST");
+  //   // const obj = JSON.parse(req.body); // FIXME: this is a bad idea. hackable from F.E.
+  //   // Token is created using Stripe Checkout or Elements!
+  //   // Get the payment token ID submitted by the form:
+  //   const token = req.body.stripeToken; // Using Express
 
-    const charge = await stripe.charges.create({
-      amount: 999,
-      currency: "usd",
-      description: "Example charge",
-      source: token,
-    });
-    if (charge.object !== "charge") {
-      res.json({ error: charge.error });
-    } else {
-      res.json({ message: "success" });
-    }
-    // return stripe.charges.create({ ...obj })
-    //   .then(() => {
-    //     console.log('good');
-    //     res.status(200).json({ response: 'done!' });
-    //   })
-    //   .catch(() => {
-    //     console.log('bad');
-    //     res.status(500).json({ response: 'payment failed' });
-    //   });
-  });
+  //   const charge = await stripe.charges.create({
+  //     amount: 999,
+  //     currency: "usd",
+  //     description: "Example charge",
+  //     source: token,
+  //   });
+  //   if (charge.object !== "charge") {
+  //     res.json({ error: charge.error });
+  //   } else {
+  //     res.json({ message: "success" });
+  //   }
+  // });
 
-  app.get("/create-payment-intent", async (req, res) => {
+  app.get("/intent", async (req, res) => {
     // const { items, currency } = req.body;
-    // Create a PaymentIntent with the order amount and currency
+
+    // in practice, get string from client, convert
+    // to an amount here, and send client_secret back
     const paymentIntent = await stripe.paymentIntents.create({
       amount: 1099,
       currency: "USD",
     });
-
-    // Send publishable key and PaymentIntent details to client
-    res.send({
-      publishableKey,
-      clientSecret: paymentIntent.client_secret,
-    });
+    sendMessageToSlack(`created a paymentIntent on server`);
+    res.json({ client_secret: paymentIntent.client_secret });
   });
 
-  app.post("/createsubscription", (req, res) => {
-    const body = JSON.parse(req.body);
-    stripe.subscriptions.create(
-      {
-        customer: body.customer,
-        items: [
-          {
-            plan: body.plan, // plan_DBMUBdi4za2guX
-          },
-        ],
-      },
-      (err, subscription) => {
-        if (!err) {
-          return res.json(subscription);
-        }
-        return res.status(400).end();
-      }
-    );
-  });
+  // app.post("/createsubscription", (req, res) => {
+  //   const body = JSON.parse(req.body);
+  //   stripe.subscriptions.create(
+  //     {
+  //       customer: body.customer,
+  //       items: [
+  //         {
+  //           plan: body.plan, // plan_DBMUBdi4za2guX
+  //         },
+  //       ],
+  //     },
+  //     (err, subscription) => {
+  //       if (!err) {
+  //         return res.json(subscription);
+  //       }
+  //       return res.status(400).end();
+  //     }
+  //   );
+  // });
 
-  app.get("/getcustomers", (req, res) => {
-    stripe.customers.list({}, async (err, customers) => {
-      if (!err) {
-        // console.log(customers);
-        return res.json({ customers: customers.data });
-      }
-      return res.status(400).end();
-    });
-  });
+  // app.get("/getcustomers", (req, res) => {
+  //   stripe.customers.list({}, async (err, customers) => {
+  //     if (!err) {
+  //       // console.log(customers);
+  //       return res.json({ customers: customers.data });
+  //     }
+  //     return res.status(400).end();
+  //   });
+  // });
 
-  app.post("/newcustomer", (req, res) => {
-    const cust = JSON.parse(req.body);
-    const source = cust.token !== null ? cust.token.id : null;
-    const userObjectToSave = {
-      firstName: cust.firstName,
-      lastName: cust.lastName,
-      email: cust.email,
-      stripe_id: null,
-    };
-    if (source) {
-      try {
-        return stripe.customers.create(
-          {
-            description: `create customer object for ${cust.email}`,
-            source, // N.B. only the ID here, not the whole token.
-            email: `${cust.email}`,
-            metadata: {
-              firstName: cust.firstName,
-              lastName: cust.lastName,
-            },
-          },
-          (err, customer) => {
-            // console.log({ err, customer });
-            if (err) return res.status(400).json({ err });
-            userObjectToSave.stripe_id = customer.id;
-            saveUser(userObjectToSave);
-            return res.status(200).json({ customer });
-          }
-        );
-      } catch (err) {
-        console.log(err); // eslint-disable-line
-        return res.status(400).json({ err });
-      }
-    } else {
-      try {
-        saveUser(userObjectToSave);
-        return res.status(200).json({ userObjectToSave });
-      } catch (mlabError) {
-        console.log(err); // eslint-disable-line
-        return res.status(400).json({ err: mlabError });
-      }
-    }
-  });
+  // app.post("/newcustomer", (req, res) => {
+  //   const cust = JSON.parse(req.body);
+  //   const source = cust.token !== null ? cust.token.id : null;
+  //   const userObjectToSave = {
+  //     firstName: cust.firstName,
+  //     lastName: cust.lastName,
+  //     email: cust.email,
+  //     stripe_id: null,
+  //   };
+  //   if (source) {
+  //     try {
+  //       return stripe.customers.create(
+  //         {
+  //           description: `create customer object for ${cust.email}`,
+  //           source, // N.B. only the ID here, not the whole token.
+  //           email: `${cust.email}`,
+  //           metadata: {
+  //             firstName: cust.firstName,
+  //             lastName: cust.lastName,
+  //           },
+  //         },
+  //         (err, customer) => {
+  //           // console.log({ err, customer });
+  //           if (err) return res.status(400).json({ err });
+  //           userObjectToSave.stripe_id = customer.id;
+  //           saveUser(userObjectToSave);
+  //           return res.status(200).json({ customer });
+  //         }
+  //       );
+  //     } catch (err) {
+  //       console.log(err); // eslint-disable-line
+  //       return res.status(400).json({ err });
+  //     }
+  //   } else {
+  //     try {
+  //       saveUser(userObjectToSave);
+  //       return res.status(200).json({ userObjectToSave });
+  //     } catch (mlabError) {
+  //       console.log(err); // eslint-disable-line
+  //       return res.status(400).json({ err: mlabError });
+  //     }
+  //   }
+  // });
 
   app.post("/webhook", async (req, res) => {
     // verify: https://stripe.com/docs/webhooks/signatures
