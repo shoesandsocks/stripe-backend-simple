5e4233a86a7bd5003fa150fc126457afbe5a18ba
RL Brown <rich@pineandvine.com>
2020-05-29 16:50:25 -0400

    missing returns?

--- routes/payment.js
+++ routes/payment.js
@@ -40,31 +40,11 @@ const sendMessageToSlack = (msg) => {
 
 const paymentApi = (app) => {
   app.get("/", (req, res) => {
-    res.send({
+    return res.send({
       message: "Hello Stripe checkout server!",
       timestamp: new Date().toISOString(),
     });
   });
-
-  // app.post("/", async (req, res) => {
-  //   console.log("handling POST");
-  //   // const obj = JSON.parse(req.body); // FIXME: this is a bad idea. hackable from F.E.
-  //   // Token is created using Stripe Checkout or Elements!
-  //   // Get the payment token ID submitted by the form:
-  //   const token = req.body.stripeToken; // Using Express
-
-  //   const charge = await stripe.charges.create({
-  //     amount: 999,
-  //     currency: "usd",
-  //     description: "Example charge",
-  //     source: token,
-  //   });
-  //   if (charge.object !== "charge") {
-  //     res.json({ error: charge.error });
-  //   } else {
-  //     res.json({ message: "success" });
-  //   }
-  // });
 
   app.get("/intent", async (req, res) => {
     // const { items, currency } = req.body;
@@ -78,80 +58,6 @@ const paymentApi = (app) => {
     // sendMessageToSlack(`created a paymentIntent on server`);
     return res.json({ client_secret: paymentIntent.client_secret });
   });
-
-  // app.post("/createsubscription", (req, res) => {
-  //   const body = JSON.parse(req.body);
-  //   stripe.subscriptions.create(
-  //     {
-  //       customer: body.customer,
-  //       items: [
-  //         {
-  //           plan: body.plan, // plan_DBMUBdi4za2guX
-  //         },
-  //       ],
-  //     },
-  //     (err, subscription) => {
-  //       if (!err) {
-  //         return res.json(subscription);
-  //       }
-  //       return res.status(400).end();
-  //     }
-  //   );
-  // });
-
-  // app.get("/getcustomers", (req, res) => {
-  //   stripe.customers.list({}, async (err, customers) => {
-  //     if (!err) {
-  //       // console.log(customers);
-  //       return res.json({ customers: customers.data });
-  //     }
-  //     return res.status(400).end();
-  //   });
-  // });
-
-  // app.post("/newcustomer", (req, res) => {
-  //   const cust = JSON.parse(req.body);
-  //   const source = cust.token !== null ? cust.token.id : null;
-  //   const userObjectToSave = {
-  //     firstName: cust.firstName,
-  //     lastName: cust.lastName,
-  //     email: cust.email,
-  //     stripe_id: null,
-  //   };
-  //   if (source) {
-  //     try {
-  //       return stripe.customers.create(
-  //         {
-  //           description: `create customer object for ${cust.email}`,
-  //           source, // N.B. only the ID here, not the whole token.
-  //           email: `${cust.email}`,
-  //           metadata: {
-  //             firstName: cust.firstName,
-  //             lastName: cust.lastName,
-  //           },
-  //         },
-  //         (err, customer) => {
-  //           // console.log({ err, customer });
-  //           if (err) return res.status(400).json({ err });
-  //           userObjectToSave.stripe_id = customer.id;
-  //           saveUser(userObjectToSave);
-  //           return res.status(200).json({ customer });
-  //         }
-  //       );
-  //     } catch (err) {
-  //       console.log(err); // eslint-disable-line
-  //       return res.status(400).json({ err });
-  //     }
-  //   } else {
-  //     try {
-  //       saveUser(userObjectToSave);
-  //       return res.status(200).json({ userObjectToSave });
-  //     } catch (mlabError) {
-  //       console.log(err); // eslint-disable-line
-  //       return res.status(400).json({ err: mlabError });
-  //     }
-  //   }
-  // });
 
   app.post("/webhook", async (req, res) => {
     // verify: https://stripe.com/docs/webhooks/signatures
--- routes/testwebhook.js
+++ routes/testwebhook.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const fetch = require('node-fetch');
-const configureStripe = require('stripe');
-const convert = require('../constants/convert');
+const fetch = require("node-fetch");
+const configureStripe = require("stripe");
+const convert = require("../constants/convert");
 
 const stripe = configureStripe(process.env.STRIPE_TEST_SECRET);
 const testEndpoint = process.env.STRIPE_TEST_ENDPOINT_SECRET;
@@ -10,9 +10,9 @@ const testEndpoint = process.env.STRIPE_TEST_ENDPOINT_SECRET;
 const sendMessageToSlack = (msg) => {
   const body = JSON.stringify({ text: convert(msg) });
   return fetch(process.env.SLACK_HOOK, {
-    method: 'POST',
+    method: "POST",
     body,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   })
     .then((response) => response.status)
     .then((status) => ({ err: null, status }))
@@ -21,18 +21,27 @@ const sendMessageToSlack = (msg) => {
 
 const testWebhookApi = (app) => {
   // TESTSTRIPE and TESTENDPOINT, instead, for test webhooks
-  app.post('/testwebhook', async (req, res) => {
-    const sig = req.headers['stripe-signature'];
+  app.post("/testwebhook", async (req, res) => {
+    const sig = req.headers["stripe-signature"];
     try {
       const event = stripe.webhooks.constructEvent(req.body, sig, testEndpoint);
       const slackReply = await sendMessageToSlack(JSON.stringify(event));
       if (slackReply.status === 200) {
-        res.status(200).send({ error: null, message: 'message sent to slack' });
+        return res
+          .status(200)
+          .send({ error: null, message: "message sent to slack" });
       }
-      res.status(200).send({ error: null, message: 'POST to /testwebhook succeeded but no Slack response' });
+      return res
+        .status(200)
+        .send({
+          error: null,
+          message: "POST to /testwebhook succeeded but no Slack response",
+        });
     } catch (err) {
       console.log(err); // eslint-disable-line
-      res.status(400).send({ error: 'POST to /testwebhook failed', message: null });
+      res
+        .status(400)
+        .send({ error: "POST to /testwebhook failed", message: null });
     }
   });
 
