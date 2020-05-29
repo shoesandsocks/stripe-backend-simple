70093f36c7519726417e0806970b8afa1e8118a6
RL Brown <rich@pineandvine.com>
2020-05-29 16:36:21 -0400

    tweak bodyparser stuff re RAW response

--- constants/frontend.js
+++ constants/frontend.js
@@ -7,6 +7,7 @@ const FRONTEND_PROD_URLS = [
   "https://dev.rich-text.net",
 ];
 
+console.log(process.env.NODE_ENV);
 module.exports =
   process.env.NODE_ENV === "production"
     ? FRONTEND_PROD_URLS
