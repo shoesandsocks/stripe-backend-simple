8a2fa1589d4e4c01f15a77f57a4489ab4a2769fc
RL Brown <rich@pineandvine.com>
2021-01-09 12:18:47 -0500

    remove all other routes

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,11 +1,11 @@
 const FRONTEND_DEV_URLS = ["http://localhost:8080", "http://localhost:3000"]; // TODO: for blog
 
 const FRONTEND_PROD_URLS = [
-  "https://www.pineandvine.com",
-  "https://pineandvine.com",
-  "https://www.rich-text.net",
-  "https://dev.rich-text.net",
-  "https://dev1.rich-text.net",
+  // "https://www.pineandvine.com",
+  // "https://pineandvine.com",
+  // "https://www.rich-text.net",
+  // "https://dev.rich-text.net",
+  // "https://dev1.rich-text.net",
   "https://admin.colorshare.club",
 ];
 
